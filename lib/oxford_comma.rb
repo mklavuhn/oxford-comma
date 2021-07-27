2b41d838f0db8d261a0db055ccdb56c435cbb65d
mklavuhn <michaelklavuhn3@gmail.com>
2021-07-27 14:26:09 -0700

    commit tree c589ba174865a859bb133522fd1e8cc691b7e8d6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.length == 1 
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else new_array = array[0...-1].join(", ")
+        new_array << ", and #{array.last}"
+        new_array
+    end
 end
